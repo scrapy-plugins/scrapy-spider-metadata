bcfd3b05e9aaf6073d0d321371c3c7cfe810c045
Adrián Chaves <adrian@chaves.io>
2023-09-14 10:52:11 +0200

    Implement get_spider_param_schema

--- scrapy_spider_metadata/__init__.py
+++ scrapy_spider_metadata/__init__.py
@@ -12,19 +12,30 @@ def _is_subclass(cls, parent):
         return False
 
 
-def parse_spider_kwargs(spider, kwargs):
-    meta = getattr(spider, "meta", None)
+def _load_param_model(spidercls, /):
+    meta = getattr(spidercls, "meta", None)
     if not isinstance(meta, Mapping):
-        return kwargs
+        return None
     param_model = meta.get("params", None)
     if not param_model:
-        return kwargs
+        return None
     if not _is_subclass(param_model, BaseModel):
-        spidercls = spider.__class__
         spider_import_path = f"{spidercls.__module__}.{spidercls.__qualname__}"
         raise ValueError(
             f'{spider_import_path}.meta["params"] is not a subclass of '
             f"pydantic.BaseModel"
         )
+    return param_model
+
+
+def get_spider_param_schema(spidercls, /):
+    param_model = _load_param_model(spidercls)
+    return param_model.model_json_schema()
+
+
+def parse_spider_kwargs(spider, kwargs, /):
+    param_model = _load_param_model(spider.__class__)
+    if param_model is None:
+        return kwargs
     parsed_params = param_model(**kwargs)
     return parsed_params.model_dump()
--- tests/test_params.py
+++ tests/test_params.py
@@ -1,10 +1,74 @@
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Field, ValidationError
 from pytest import raises
 from scrapy import Spider
 
-from scrapy_spider_metadata import parse_spider_kwargs
+from scrapy_spider_metadata import get_spider_param_schema, parse_spider_kwargs
 
 from . import get_spider
+
+
+def test_get_spider_param_schema():
+    pattern = r"^(\\([0-9]{3}\\))?[0-9]{3}-[0-9]{4}$"
+
+    class Params(BaseModel):
+        field: int = Field(
+            title="A Team",
+            description="This is a description of the A team.",
+            json_schema_extra={
+                "foo": "bar",
+            },
+            default=0,
+        )
+        int_with_default: int = 1
+        number_without_default: float
+        phone: str = Field(
+            min_length=3,
+            max_length=100,
+            pattern=pattern,
+        )
+        yesno: bool
+
+    class ParamSpider(Spider):
+        name = "params"
+        meta = {
+            "params": Params,
+        }
+
+    schema = get_spider_param_schema(ParamSpider)
+    assert schema == {
+        "properties": {
+            "field": {
+                "title": "A Team",
+                "description": "This is a description of the A team.",
+                "type": "integer",
+                "foo": "bar",
+                "default": 0,
+            },
+            "int_with_default": {
+                "title": "Int With Default",
+                "type": "integer",
+                "default": 1,
+            },
+            "number_without_default": {
+                "title": "Number Without Default",
+                "type": "number",
+            },
+            "phone": {
+                "title": "Phone",
+                "type": "string",
+                "minLength": 3,
+                "maxLength": 100,
+                "pattern": pattern,
+            },
+            "yesno": {
+                "title": "Yesno",
+                "type": "boolean",
+            },
+        },
+        "required": ["number_without_default", "phone", "yesno"],
+        "title": "Params",
+        "type": "object",
+    }
 
 
 def test_parse_spider_kwargs_convert():
